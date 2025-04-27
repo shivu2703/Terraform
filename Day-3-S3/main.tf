78cc7de9ee631b99bf5d46702a26d7646da11734
Shivangi Katiyar <shga2797@gmail.com>
2025-04-27 19:32:51 +0000

    Removing the Day-3-S3 folder since created that under modules folder

--- Day-3-S3/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "s3_bucket" {
-  source = "./module/s3bucket"
-  bucket_name = "terraform-s3-test-bucket-shivu"
-}
--- Day-3-S3/module/s3bucket/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "bucket_name" {
-    description = "name of the bucket"
-}
